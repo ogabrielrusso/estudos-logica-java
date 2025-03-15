32bb0e92fc6e481b318c31bd84682c47358be2b1
gabrieldevdesigner <gabrieldevdesigner@proton.me>
2025-03-15 18:35:12 +0000

    Novas sugestões

---
+++ Novos Códigos/ex01.java
@@ -0,0 +1,7 @@
+/*Declare uma variável soma, para armazenar a soma dos números.
+Crie uma lista de inteiros chamada numeros.
+Adiciona cinco números à lista.
+Itera sobre a lista e calcule a soma dos números.
+Exiba a soma dos valores na lista.*/
+
+
